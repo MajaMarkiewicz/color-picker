5703fd1095c959d38cee0356f6f2ca488c6ab6a7
MajaMarkiewicz <markiewicz.maja@gmail.com>
2021-03-17 17:44:19 +0100

    Config svg

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   transform: {
-    '^.+\\.vue$': 'vue-jest'
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.svg$': './svgTransform.js'
   }
 }
